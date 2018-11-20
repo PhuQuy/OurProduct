bb81cdcdc802f586db38296e79a7e14d2672e180
CaptainLee <17521098@gm.uit.edu.vn>
2018-11-20 08:28:10 +0700

    Update footer, new-templates, download-card

--- src/app/components/model/download-card/download-card.component.ts
+++ src/app/components/model/download-card/download-card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { DownloadCard } from '../../../model/cards/download-card/download-card.module'
 @Component({
   selector: 'download-card',
   templateUrl: './download-card.component.html',
   styleUrls: ['./download-card.component.scss']
 })
 export class DownloadCardComponent implements OnInit {
-
+  @Input() public downloadCard: DownloadCard
   constructor() { }
-
+  public imgUrl : string;
   ngOnInit() {
+
   }
 
 }
--- src/app/components/templates/new-templates/new-templates.component.ts
+++ src/app/components/templates/new-templates/new-templates.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DownloadCard } from '../../../model/cards/download-card/download-card.module'
 @Component({
   selector: 'new-templates',
   templateUrl: './new-templates.component.html',
@@ -8,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class NewTemplatesComponent implements OnInit {
 
   constructor() { }
-
+  public cards: Array<DownloadCard>
+  public card: DownloadCard
+  public active: string
   ngOnInit() {
+    this.cards = [
+      new DownloadCard('Card Title', 'HTML Template', 3, 300, "https://dummyimage.com/286x180/8f078f/0011ff.png"),
+      new DownloadCard('Card Title', 'UI Design', 3, 300, "https://dummyimage.com/286x180/8f078f/0011ff.png"),
+      new DownloadCard('Card Title', 'WorldPress', 3, 300, "https://dummyimage.com/286x180/8f078f/0011ff.png"),
+      new DownloadCard('Card Title', 'HTML Template', 3, 300, "https://dummyimage.com/286x180/8f078f/0011ff.png")
+    ]
+    this.active = '';
+    console.log(this.cards)
   }
-
+  filterItemsOfType(type) {
+    if (type === '') return this.cards
+    return this.cards.filter(x => x.type === type);
+    // return this.cards
+  }
 }
